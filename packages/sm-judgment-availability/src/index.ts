a60084ec287b8e904d4769bbf94d5c3c20145180
takuma531zov <takumalshikura531@gmail.com>
2025-03-01 13:31:17 +0900

    setup

--- packages/sm-judgment-availability/src/index.ts
+++ packages/sm-judgment-availability/src/index.ts
@@ -1,8 +1,9 @@
-function myFunction() {
+export function myFunction() {
   //1.スプレッドシートのデータ取得
   //指定した範囲のデータを取得し、各行ごとにDify APIにリクエストを送る。
   //2.Dify APIへのリクエスト
   //Difyを用いて各行のデータの整合性を判定し、結果を取得。
   //3.判定結果をスプレッドシートに書き込む
   //4.falseのデータを別シートに保存
+  console.log("Hello");
 }
